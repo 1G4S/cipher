b9f6d86ac0d9cc7453416fd735c43c042a2114ef
1G4S <igor.sarnowski.21@gmail.com>
2024-05-30 11:10:35 +0200

    feat(rot47): add encrypt and decrypt functions for ROT47

--- src/hashers/rot47.py
+++ src/hashers/rot47.py
@@ -3,7 +3,13 @@ from src.hashers.cipher import Cipher
 
 class ROT47(Cipher):
     def encrypt(self, text: str) -> str:
-        pass
+        rot47_text = r"""!"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~"""
+        return text.translate(str.maketrans(
+            rot47_text,
+            rot47_text[47:] + rot47_text[:47]))
 
     def decrypt(self, text: str) -> str:
-        pass
+        rot47_text = r"""!"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~"""
+        return text.translate(str.maketrans(
+            rot47_text[47:] + rot47_text[:47],
+            rot47_text))
