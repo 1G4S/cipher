1f5ad4f38aec5919284ef64e6154fa9713c27a90
1G4S <igor.sarnowski.21@gmail.com>
2024-05-30 09:53:30 +0200

    test(rot47): add unit tests for ROT47 encrypt and decrypt methods

--- src/hashers/rot47.py
+++ src/hashers/rot47.py
@@ -2,8 +2,8 @@ from src.hashers.cipher import Cipher
 
 
 class ROT47(Cipher):
-    def encrypt(self):
+    def encrypt(self, text: str) -> str:
         pass
 
-    def decrypt(self):
+    def decrypt(self, text: str) -> str:
         pass
