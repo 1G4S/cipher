a204191b6c025ea894ea5054ffe16780950e86fb
1G4S <igor.sarnowski.21@gmail.com>
2024-05-30 19:39:16 +0200

    feat(rot13): add validation method to ROT13

--- src/hashers/rot13.py
+++ src/hashers/rot13.py
@@ -3,13 +3,23 @@ from src.hashers.cipher import Cipher
 
 class ROT13(Cipher):
     def encrypt(self, text: str) -> str:
+        if self.validation(text):
+            raise ValueError
+
         return text.translate(str.maketrans(
             "ABCDEFGHIJKLMabcdefghijklmNOPQRSTUVWXYZnopqrstuvwxyz",
             "NOPQRSTUVWXYZnopqrstuvwxyzABCDEFGHIJKLMabcdefghijklm"
         ))
 
     def decrypt(self, text: str) -> str:
+        if self.validation(text):
+            raise ValueError
+
         return text.translate(str.maketrans(
             "NOPQRSTUVWXYZnopqrstuvwxyzABCDEFGHIJKLMabcdefghijklm",
             "ABCDEFGHIJKLMabcdefghijklmNOPQRSTUVWXYZnopqrstuvwxyz"
         ))
+
+    @staticmethod
+    def validation(text: str) -> bool:
+        return any(str(i) in text for i in range(10))
--- src/run.py
+++ src/run.py
@@ -1,6 +1,6 @@
 import unittest
 
 loader = unittest.TestLoader()
-tests = loader.discover('/Users/igorsarnowski/PycharmProjects/cipher/tests/files')
+tests = loader.discover('/Users/igorsarnowski/PycharmProjects/cipher/tests/hashers')
 testRunner = unittest.TextTestRunner()
 testRunner.run(tests)
