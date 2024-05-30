01b819636cf9c400bc3f9a4e014973b11227ef87
1G4S <igor.sarnowski.21@gmail.com>
2024-05-30 19:45:41 +0200

    fix(tests): fix typing in unit tests for ROT13 and fix names of test files for ROT13 and ROT47

--- tests/hashers/test_rot13.py
+++ tests/hashers/test_rot13.py
@@ -7,18 +7,18 @@ class TestRot13(unittest.TestCase):
         self.rot13 = ROT13()
 
     def test_encrypt_rot13(self):
-        before_encrypt1 = "arbuz"
-        before_encrypt2 = "ptaki lataja kluczem"
-        before_encrypt3 = "project errors"
+        before_encrypt1: str = "arbuz"
+        before_encrypt2: str = "ptaki lataja kluczem"
+        before_encrypt3: str = "project errors"
 
         self.assertEqual(self.rot13.encrypt(text=before_encrypt1), "neohm")
         self.assertEqual(self.rot13.encrypt(text=before_encrypt2), "cgnxv yngnwn xyhpmrz")
         self.assertEqual(self.rot13.encrypt(text=before_encrypt3), "cebwrpg reebef")
 
     def test_decrypt_rot13(self):
-        before_decrypt1 = "neohm"
-        before_decrypt2 = "cgnxv yngnwn xyhpmrz"
-        before_decrypt3 = "cebwrpg reebef"
+        before_decrypt1: str = "neohm"
+        before_decrypt2: str = "cgnxv yngnwn xyhpmrz"
+        before_decrypt3: str = "cebwrpg reebef"
 
         self.assertEqual(self.rot13.decrypt(text=before_decrypt1), "arbuz")
         self.assertEqual(self.rot13.decrypt(text=before_decrypt2), "ptaki lataja kluczem")
