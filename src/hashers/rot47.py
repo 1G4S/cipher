294071176a19ca4898ec554453659140682f03d1
1G4S <igor.sarnowski.21@gmail.com>
2024-05-30 11:13:08 +0200

    fix(rot47): fix typing in ROT47

--- src/hashers/rot47.py
+++ src/hashers/rot47.py
@@ -3,13 +3,13 @@ from src.hashers.cipher import Cipher
 
 class ROT47(Cipher):
     def encrypt(self, text: str) -> str:
-        rot47_text = r"""!"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~"""
+        rot47_text: str = r"""!"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~"""
         return text.translate(str.maketrans(
             rot47_text,
             rot47_text[47:] + rot47_text[:47]))
 
     def decrypt(self, text: str) -> str:
-        rot47_text = r"""!"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~"""
+        rot47_text: str = r"""!"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~"""
         return text.translate(str.maketrans(
             rot47_text[47:] + rot47_text[:47],
             rot47_text))
