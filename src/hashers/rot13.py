94d29f3e09cf60172e01fbd2a70e03acf0152ba3
1G4S <igor.sarnowski.21@gmail.com>
2024-06-02 14:55:18 +0200

    Changes by admin

--- src/hashers/rot13.py
+++ src/hashers/rot13.py
@@ -2,23 +2,26 @@ from src.hashers.cipher import Cipher
 
 
 class ROT13(Cipher):
+    _enc_table = str.maketrans(
+        "ABCDEFGHIJKLMabcdefghijklmNOPQRSTUVWXYZnopqrstuvwxyz",
+        "NOPQRSTUVWXYZnopqrstuvwxyzABCDEFGHIJKLMabcdefghijklm",
+    )
+    _dec_table = str.maketrans(
+        "NOPQRSTUVWXYZnopqrstuvwxyzABCDEFGHIJKLMabcdefghijklm",
+        "ABCDEFGHIJKLMabcdefghijklmNOPQRSTUVWXYZnopqrstuvwxyz",
+    )
+
     def encrypt(self, text: str) -> str:
         if self.validation(text):
             raise ValueError
 
-        return text.translate(str.maketrans(
-            "ABCDEFGHIJKLMabcdefghijklmNOPQRSTUVWXYZnopqrstuvwxyz",
-            "NOPQRSTUVWXYZnopqrstuvwxyzABCDEFGHIJKLMabcdefghijklm"
-        ))
+        return text.translate(self._enc_table)
 
     def decrypt(self, text: str) -> str:
         if self.validation(text):
             raise ValueError
 
-        return text.translate(str.maketrans(
-            "NOPQRSTUVWXYZnopqrstuvwxyzABCDEFGHIJKLMabcdefghijklm",
-            "ABCDEFGHIJKLMabcdefghijklmNOPQRSTUVWXYZnopqrstuvwxyz"
-        ))
+        return text.translate(self._dec_table)
 
     @staticmethod
     def validation(text: str) -> bool:
