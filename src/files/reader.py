5d5eab41d29d82adb3df3d75b35236ebc9e77f85
1G4S <igor.sarnowski.21@gmail.com>
2024-06-02 21:07:14 +0200

    chore(files): remove saver and reader

--- src/files/reader.py
+++
@@ -1,4 +0,0 @@
-class Reader:
-    @staticmethod
-    def read_from_file(path: str) -> str:
-        pass
--- src/files/saver.py
+++
@@ -1,5 +0,0 @@
-class Saver:
-    @staticmethod
-    def save_text_to_file(text: str, filename: str) -> None:
-        with open(filename, "w") as file:
-            file.write(text)
