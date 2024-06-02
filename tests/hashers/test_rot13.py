853855a535ea81747774d1192d7757cc6f0a21d4
1G4S <igor.sarnowski.21@gmail.com>
2024-06-02 15:21:36 +0200

    chore(hashers): change structure of unit tests by by splitting the tests into several

--- tests/hashers/test_rot13.py
+++ tests/hashers/test_rot13.py
@@ -6,24 +6,26 @@ class TestRot13(unittest.TestCase):
     def setUp(self):
         self.rot13 = ROT13()
 
-    def test_encrypt_rot13(self):
+    def test_encrypt_rot13_for_single_word(self):
         before_encrypt1 = "arbuz"
-        before_encrypt2 = "ptaki lataja kluczem"
-        before_encrypt3 = "project errors"
-
         self.assertEqual(self.rot13.encrypt(text=before_encrypt1), "neohm")
-        self.assertEqual(
-            self.rot13.encrypt(text=before_encrypt2), "cgnxv yngnwn xyhpmrz"
-        )
+
+    def test_encrypt_rot13_for_several_words_first_type(self):
+        before_encrypt2 = "ptaki lataja kluczem"
+        self.assertEqual(self.rot13.encrypt(text=before_encrypt2), "cgnxv yngnwn xyhpmrz")
+
+    def test_encrypt_rot13_for_several_words_second_type(self):
+        before_encrypt3 = "project errors"
         self.assertEqual(self.rot13.encrypt(text=before_encrypt3), "cebwrpg reebef")
 
-    def test_decrypt_rot13(self):
+    def test_decrypt_rot13_for_single_word(self):
         before_decrypt1 = "neohm"
-        before_decrypt2 = "cgnxv yngnwn xyhpmrz"
-        before_decrypt3 = "cebwrpg reebef"
-
         self.assertEqual(self.rot13.decrypt(text=before_decrypt1), "arbuz")
-        self.assertEqual(
-            self.rot13.decrypt(text=before_decrypt2), "ptaki lataja kluczem"
-        )
+
+    def test_decrypt_rot13_for_several_words_first_type(self):
+        before_decrypt2 = "cgnxv yngnwn xyhpmrz"
+        self.assertEqual(self.rot13.decrypt(text=before_decrypt2), "ptaki lataja kluczem")
+
+    def test_decrypt_rot13_for_several_words_second_type(self):
+        before_decrypt3 = "cebwrpg reebef"
         self.assertEqual(self.rot13.decrypt(text=before_decrypt3), "project errors")
--- tests/hashers/test_rot47.py
+++ tests/hashers/test_rot47.py
@@ -6,20 +6,26 @@ class TestRot47(unittest.TestCase):
     def setUp(self):
         self.rot47 = ROT47()
 
-    def test_encrypt_rot47(self):
+    def test_encrypt_rot47_for_single_word(self):
         before_encrypt1 = "arbuz"
-        before_encrypt2 = "ptaki lataja kluczem"
-        before_encrypt3 = "project errors"
-
         self.assertEqual(self.rot47.encrypt(before_encrypt1), "2C3FK")
+
+    def test_encrypt_rot47_for_several_words_first_type(self):
+        before_encrypt2 = "ptaki lataja kluczem"
         self.assertEqual(self.rot47.encrypt(before_encrypt2), "AE2<: =2E2;2 <=F4K6>")
+
+    def test_encrypt_rot47_for_several_words_second_type(self):
+        before_encrypt3 = "project errors"
         self.assertEqual(self.rot47.encrypt(before_encrypt3), "AC@;64E 6CC@CD")
 
-    def test_decrypt_rot47(self):
+    def test_decrypt_rot47_for_single_word(self):
         before_decrypt1 = "2C3FK"
-        before_decrypt2 = "AE2<: =2E2;2 <=F4K6>"
-        before_decrypt3 = "AC@;64E 6CC@CD"
-
         self.assertEqual(self.rot47.decrypt(before_decrypt1), "arbuz")
+
+    def test_decrypt_rot47_for_several_words_first_type(self):
+        before_decrypt2 = "AE2<: =2E2;2 <=F4K6>"
         self.assertEqual(self.rot47.decrypt(before_decrypt2), "ptaki lataja kluczem")
+
+    def test_decrypt_rot47_for_several_words_second_type(self):
+        before_decrypt3 = "AC@;64E 6CC@CD"
         self.assertEqual(self.rot47.decrypt(before_decrypt3), "project errors")
