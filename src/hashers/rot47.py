6d4c4005456323fdb0f43d948505e32ff7c2648a
1G4S <igor.sarnowski.21@gmail.com>
2024-06-02 15:47:49 +0200

    feat(rot47): add validation for polish signs in ROT47

--- src/hashers/rot47.py
+++ src/hashers/rot47.py
@@ -2,14 +2,25 @@ from src.hashers.cipher import Cipher
 
 
 class ROT47(Cipher):
+    _enc_dec_table: str = r"""!"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~"""
+
     def encrypt(self, text: str) -> str:
-        rot47_text: str = r"""!"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~"""
+        if self.validation(text):
+            raise ValueError
+
         return text.translate(str.maketrans(
-            rot47_text,
-            rot47_text[47:] + rot47_text[:47]))
+            self._enc_dec_table,
+            self._enc_dec_table[47:] + self._enc_dec_table[:47]))
 
     def decrypt(self, text: str) -> str:
-        rot47_text: str = r"""!"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~"""
+        if self.validation(text):
+            raise ValueError
+        
         return text.translate(str.maketrans(
-            rot47_text[47:] + rot47_text[:47],
-            rot47_text))
+            self._enc_dec_table[47:] + self._enc_dec_table[:47],
+            self._enc_dec_table))
+
+    @staticmethod
+    def validation(text: str) -> bool:
+        wrong_chars: str = "ĄąĆćĘęŁłŃńÓóŚśŹźŻż"
+        return any(i in text for i in wrong_chars)
