1022428f0e1b4ba131938b8b27914d5fd6119836
1G4S <igor.sarnowski.21@gmail.com>
2024-06-02 14:49:29 +0200

    chore(run_unit_test): add run unit test file for testing methods

--- src/run.py
+++
@@ -1,6 +0,0 @@
-import unittest
-
-loader = unittest.TestLoader()
-tests = loader.discover('/Users/igorsarnowski/PycharmProjects/cipher/tests/hashers')
-testRunner = unittest.TextTestRunner()
-testRunner.run(tests)
