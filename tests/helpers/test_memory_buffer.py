f296e3c12e5ae8eeec75269a5a2a0f438f747fcc
1G4S <igor.sarnowski.21@gmail.com>
2024-06-04 20:47:19 +0200

    test(memory_buffer): add unit tests for remove and add text in memorybuffer

---
+++ tests/helpers/test_memory_buffer.py
@@ -0,0 +1,30 @@
+import unittest
+from src.hashers.text import Text
+from src.helpers.memory_buffer import MemoryBuffer
+
+
+class TestMemoryBuffer(unittest.TestCase):
+    def test_add_text(self):
+        buffer = MemoryBuffer()
+        text1 = Text("12345", "rot13", "encrypted")
+        text2 = Text("31254215", "rot47", "decrypted")
+
+        buffer.add_text(text1)
+        buffer.add_text(text2)
+
+        self.assertEqual(len(buffer.buffer), 2)
+        self.assertIn(text1, buffer.buffer)
+        self.assertIn(text2, buffer.buffer)
+
+    def test_remove_text(self):
+        buffer = MemoryBuffer()
+        text1 = Text("12345", "rot13", "encrypted")
+        text2 = Text("31254215", "rot47", "decrypted")
+        buffer.add_text(text1)
+        buffer.add_text(text2)
+
+        buffer.remove_text("12345")
+
+        self.assertEqual(len(buffer.buffer), 1)
+        self.assertNotIn(text1, buffer.buffer)
+        self.assertIn(text2, buffer.buffer)
