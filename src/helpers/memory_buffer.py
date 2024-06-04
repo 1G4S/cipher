b6a61471197f62ce9dae20b29c9644339dc7f407
1G4S <igor.sarnowski.21@gmail.com>
2024-06-04 20:55:24 +0200

    feat(memory_buffer): add validation in MemoryBuffer

--- src/helpers/memory_buffer.py
+++ src/helpers/memory_buffer.py
@@ -6,12 +6,23 @@ class MemoryBuffer:
         self.buffer: list[Text] = []
 
     def add_text(self, data: Text) -> None:
+        if self.validation(data=data):
+            raise ValueError
+        
         self.buffer.append(data)
 
     def remove_text(self, name: str) -> None:
         for text in self.buffer.copy():
             if name == text.text:
                 self.buffer.remove(text)
+
+    @staticmethod
+    def validation(data: Text) -> bool:
+        if (data.rot_type != "rot13" and data.rot_type != "rot47") or (
+                data.status != "encrypted" and data.status != "decrypted"
+        ):
+            return True
+        return False
 
     def __str__(self):
         print(self.buffer)
