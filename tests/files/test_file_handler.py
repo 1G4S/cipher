487c2157e7d2774a16fd00f76207c90cb6573c04
1G4S <igor.sarnowski.21@gmail.com>
2024-06-08 09:40:46 +0200

    test(file_handler): add unit tests for save and read functions and add unit tests for validate function

--- tests/files/test_file_handler.py
+++ tests/files/test_file_handler.py
@@ -1,4 +1,3 @@
-import json
 import unittest
 import os
 from src.files.file_handler import FileHandler
@@ -8,7 +7,10 @@ from src.hashers.text import Text
 class TestFileHandler(unittest.TestCase):
     def setUp(self):
         self.test_file = "test_output.json"
-        self.test_data = Text("str", "rot13", "encrypted")
+        self.test_data = [Text("str", "rot13", "encrypted"), Text('str', 'rot47', 'encrypted'),
+                          Text('st1234', 'rot13', 'encrypted')]
+        self.test_data_to_read_from_file = [Text("str", "rot13", "encrypted"), Text('str12', 'rot13', 'decrypted'),
+                                            Text('str32', 'rot47', 'encrypted')]
 
     def tearDown(self):
         if os.path.exists(self.test_file):
@@ -20,21 +22,17 @@ class TestFileHandler(unittest.TestCase):
 
     def test_save_text_to_file_content_in_file_handler(self):
         FileHandler.save_text_to_file(data=self.test_data, filename=self.test_file)
-
-        with open(self.test_file) as file:
-            data = json.load(file)
-        data_read = Text(data["text"], data["rot_type"], data["status"])
+        data_read = FileHandler.read_from_file(self.test_file)
         self.assertEqual(data_read, self.test_data)
 
     def test_read_from_file_in_file_handler(self):
-        data = Text("str", "rot13", "encrypted")
         data_read = FileHandler.read_from_file(path="test_read.json")
-        self.assertEqual(data, data_read)
+        self.assertEqual(self.test_data_to_read_from_file, data_read)
 
-    def test_validation_in_file_handler_for_right_value(self):
-        data_to_validate = Text("str", "rot13", "encrypted")
-        self.assertEqual(FileHandler.validation(data=data_to_validate), False)
+    def test_is_filename_valid_for_right_value(self):
+        filename = "python.json"
+        self.assertEqual(FileHandler.is_filename_valid(filename), True)
 
-    def test_validation_in_file_handler_for_wrong_value(self):
-        data_to_validate = Text("str", "rot13", "encrypteerrd")
-        self.assertEqual(FileHandler.validation(data=data_to_validate), True)
+    def test_is_filename_valid_for_wrong_value(self):
+        filename = "python.jsn"
+        self.assertEqual(FileHandler.is_filename_valid(filename), False)
