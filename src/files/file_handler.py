4e6399f0c23d040acafb9085bb57a89f87cca99e
1G4S <igor.sarnowski.21@gmail.com>
2024-06-08 08:34:39 +0200

    feat(file_handler): add validation for filename and change saving function that can use list to save

--- src/files/file_handler.py
+++ src/files/file_handler.py
@@ -1,21 +1,28 @@
 import json
+import re
+
 from src.hashers.text import Text
+from dataclasses import asdict
 
 
 class FileHandler:
 
     @staticmethod
-    def save_text_to_file(data: Text, filename: str) -> None:
-        if FileHandler.validation(data):
-            raise ValueError
+    def save_text_to_file(data: list[Text], filename: str) -> None:
+        if not FileHandler.is_filename_valid(filename=filename):
+            raise ValueError("Filename is not correct. It must end with .json")
 
-        formatted_data: dict = {
-            "text": data.text,
-            "rot_type": data.rot_type,
-            "status": data.status,
-        }
+        formatted_text_list = []
+        formatted_dict = {}
+        for d in data:
+            if d.is_text_valid():
+                formatted_text_list.append(asdict(d))
+            else:
+                raise ValueError
+        formatted_dict['data'] = formatted_text_list
+
         with open(filename, "w") as file:
-            json.dump(formatted_data, file)
+            json.dump(formatted_dict, file, indent=4)
 
     @staticmethod
     def read_from_file(path: str) -> Text:
@@ -30,7 +37,14 @@ class FileHandler:
     @staticmethod
     def validation(data: Text) -> bool:
         if (data.rot_type != "rot13" and data.rot_type != "rot47") or (
-            data.status != "encrypted" and data.status != "decrypted"
+                data.status != "encrypted" and data.status != "decrypted"
         ):
+            return True
+        return False
+
+    @staticmethod
+    def is_filename_valid(filename: str) -> bool:
+        pattern = re.compile("[A-z0-9]+.json")
+        if pattern.match(filename):
             return True
         return False
