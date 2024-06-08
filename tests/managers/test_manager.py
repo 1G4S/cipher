bf2ec15b42c7fadc4fac58027bbe4f9f52a47039
1G4S <igor.sarnowski.21@gmail.com>
2024-06-08 22:59:51 +0200

    test(manager): add unit test for encrypt_rot13 in manager

--- tests/managers/test_manager.py
+++ tests/managers/test_manager.py
@@ -1,11 +1,17 @@
 import unittest
 
+from src.helpers.text import Text
 from src.managers.manager import Manager
+from unittest.mock import patch, MagicMock
 
 
 class TestManager(unittest.TestCase):
     def setUp(self):
-        manager = Manager()
+        self.manager = Manager()
 
-    def test_encrypt_rot13_in_manager(self):
-        ...
+    @patch("builtins.input", return_value="arbuz")
+    def test_encrypt_rot13_in_manager(self, mock_input):
+        text = Text("neohm", "rot13", "encrypted")
+        self.manager.encrypt_rot13()
+        self.assertEqual(self.manager.memory.buffer[0], text)
+        self.manager.clear_memory()
