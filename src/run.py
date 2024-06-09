4ec3fb7c1fd20f66d756672994fb7970636ce31b
1G4S <igor.sarnowski.21@gmail.com>
2024-06-09 12:27:20 +0200

    refactor(run): change structure of run.py

--- src/run.py
+++ src/run.py
@@ -1,4 +1,12 @@
+from src.hashers.rot13 import ROT13
+from src.hashers.rot47 import ROT47
+from src.helpers.memory_buffer import MemoryBuffer
 from src.managers.manager import Manager
+from src.menus.menu import Menu
 
-manager = Manager()
+memory_buffer = MemoryBuffer()
+rot13 = ROT13()
+rot47 = ROT47()
+menu = Menu({})
+manager = Manager(memory_buffer=memory_buffer, rot13=rot13, rot47=rot47, menu=menu)
 manager.main_loop()
