afa5df10742840541a02f0f92a89482958f7316c
1G4S <igor.sarnowski.21@gmail.com>
2024-06-09 12:46:45 +0200

    feat(text): add __str_

--- src/helpers/text.py
+++ src/helpers/text.py
@@ -12,14 +12,18 @@ class Text:
 
     @staticmethod
     def is_text_from_dict_valid(data: dict[str, str]) -> bool:
-        return Text.__is_valid(data.get('rot_type'), data.get('status'))
+        return Text.__is_valid(data.get("rot_type"), data.get("status"))
 
     @staticmethod
     def __is_valid(rot_type: str, status: str) -> bool:
-        return rot_type in {'rot13', 'rot47'} and status in {'encrypted', 'decrypted'}
+        return rot_type in {"rot13", "rot47"} and status in {"encrypted", "decrypted"}
 
     @staticmethod
     def from_dict(data: dict) -> "Text":
         if not Text.is_text_from_dict_valid(data=data):
             raise ValueError
         return Text(**data)
+
+    def __str__(self):
+        return f'Text: {self.text}, Type: {self.rot_type}, Status: {self.status}'
+
