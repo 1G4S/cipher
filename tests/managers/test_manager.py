a6882ab061ad95a22406c26ba3a7c73638ee3c5f
1G4S <igor.sarnowski.21@gmail.com>
2024-06-09 14:33:32 +0200

    refactor(test_manager): refactor unit test for remove element

--- tests/managers/test_manager.py
+++ tests/managers/test_manager.py
@@ -29,28 +29,28 @@ class TestManager(unittest.TestCase):
         text = Text("neohm", "rot13", "encrypted")
         self.manager.encrypt_rot13()
         self.assertEqual(self.manager.memory.buffer[0], text)
-        self.manager.clear_memory()
+        self.manager.memory.buffer = []
 
     @patch("builtins.input", return_value="neohm")
     def test_decrypt_rot13_in_manager(self, mock_input):
         text = Text("arbuz", "rot13", "decrypted")
         self.manager.decrypt_rot13()
         self.assertEqual(self.manager.memory.buffer[0], text)
-        self.manager.clear_memory()
+        self.manager.memory.buffer = []
 
     @patch("builtins.input", return_value="project errors")
     def test_encrypt_rot47_in_manager(self, mock_input):
         text = Text("AC@;64E 6CC@CD", "rot47", "encrypted")
         self.manager.encrypt_rot47()
         self.assertEqual(self.manager.memory.buffer[0], text)
-        self.manager.clear_memory()
+        self.manager.memory.buffer = []
 
     @patch("builtins.input", return_value="AC@;64E 6CC@CD")
     def test_decrypt_rot47_in_manager(self, mock_input):
         text = Text("project errors", "rot47", "decrypted")
         self.manager.decrypt_rot47()
         self.assertEqual(self.manager.memory.buffer[0], text)
-        self.manager.clear_memory()
+        self.manager.memory.buffer = []
 
     @patch('sys.stdout', new_callable=StringIO)
     def test_display_memory_buffer_in_manager(self, mock_out):
@@ -59,7 +59,7 @@ class TestManager(unittest.TestCase):
         self.manager.memory.add_text(text)
         self.manager.display_memory_buffer()
         self.assertIn(t, mock_out.getvalue())
-        self.manager.clear_memory()
+        self.manager.memory.buffer = []
 
     @patch('builtins.input', return_value="/Users/igorsarnowski/PycharmProjects/cipher/tests/test.json")
     def test_read_from_file_in_manager(self, mock_input):
@@ -67,7 +67,7 @@ class TestManager(unittest.TestCase):
         test_list = [text]
         self.manager.read_from_file()
         self.assertEqual(self.manager.memory.buffer, test_list)
-        self.manager.clear_memory()
+        self.manager.memory.buffer = []
 
     @patch('builtins.input',
            side_effect=["test.json", "/Users/igorsarnowski/PycharmProjects/cipher/tests/test.json"])
@@ -76,15 +76,17 @@ class TestManager(unittest.TestCase):
         test_list = [text]
         self.manager.memory.add_text(text)
         self.manager.save_to_file()
-        self.manager.clear_memory()
+        self.manager.memory.buffer = []
         self.manager.read_from_file()
         self.assertEqual(self.manager.memory.buffer, test_list)
-        self.manager.clear_memory()
+        self.manager.memory.buffer = []
 
-    @patch("builtins.input", return_value="neohm")
-    def test_clear_memory(self, mock_input):
+    @patch("builtins.input", side_effect=["neohm", "neohm", 1, 1])
+    def test_remove_from_memory(self, mock_input):
         self.manager.encrypt_rot13()
-        self.manager.clear_memory()
+        self.manager.encrypt_rot13()
+        self.manager.remove_element()
+        self.manager.remove_element()
         self.assertEqual(len(self.manager.memory.buffer), 0)
 
     def test_exit_program(self):
